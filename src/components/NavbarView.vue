1084bd9789f4eb4aabc13bbf3853885bfdaeb59d
masaris888 <malfarizipratama62@gmail.com>
2025-06-09 19:23:53 +0700

    membuat component navbar

--- src/components/NavbarView.vue
+++ src/components/NavbarView.vue
@@ -1,4 +1,5 @@
- <template>
+<!-- navbar -->
+<template>
   <nav class="bg-green-600 text-white p-4 flex justify-between items-center">
     <h1 class="text-xl font-bold">KostKu</h1>
     <div class="space-x-4">
@@ -9,6 +10,7 @@
     </div>
   </nav>
 </template>
+<!-- end navbar -->
 
 
 <script>
